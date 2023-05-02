e7489244479dddaedbab9df6d4773a8dbf829896
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-02 07:28:39 +0600

    route setup

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,17 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import ChefRecipe from "../Pages/ChefRecipe/ChefRecipe/ChefRecipe";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const route = createBrowserRouter([
     {
         path: '/',
         element: <Main/>,
         children: [
-        {
-          path: '/',
-          element: aa,
-        },
-      ],
-    },
-  ]);
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: 'chefRecipe/:id',
+                element: <ChefRecipe/>,
+                loader: ({params}) => fetch(`http://localhost:4000/recipe/${params.id}`)
+            },
+            {
+                path: 'login',
+                element: <Login/>
+            },
+            {
+                path: 'register',
+                element: <Register/>
+            }
+        ]
+    }
+])
 
 export default route;
