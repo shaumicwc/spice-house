ecc24ac3108456bad174b245865510a8b85e47f2
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-02 08:17:44 +0600

    chef recipe component updated

--- src/Components/Pages/ChefRecipe/ChefRecipe/ChefRecipe.jsx
+++ src/Components/Pages/ChefRecipe/ChefRecipe/ChefRecipe.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Recipe from '../Recipe/Recipe';
 
 const ChefRecipe = () => {
+    const data = useLoaderData();
+    const { id, name, picture, yearsOfExperience, numberOfRecipes, likes, bio, recipes } = data;
+
     return (
-        <div>
-            
+        <div className='md:mx-16'>
+            <div className='flex justify-between w-full bg-slate-300 h-full'>
+                <div className='m-16 w-5/12'>
+                    <p className='text-2xl font-bold mb-5'>{name}</p>
+                    <p className='font-semibold'>Years of experience : <span className='font-normal'>{yearsOfExperience} years</span></p>
+                    <p className='font-semibold'>Number of Recipes : <span className='font-normal'>{numberOfRecipes} recipes</span></p>
+                    <p className='font-semibold'>Bio : <span className='font-normal'>{bio}</span></p>
+                    <p className='font-semibold'>Likes : <span className='font-normal'>{likes}</span></p>
+                </div>
+                <img className='w-2/6' src={picture} alt="" />
+            </div>
+                <table className="w-full mt-5">
+                    <thead className='bg-table-color'>
+                        <tr>
+                            <th className='p-3'>NAME</th>
+                            <th>INGREDIENTS</th>
+                            <th>COKING METHOD</th>
+                            <th>RATINGS</th>
+                            <th>FAVORITE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            recipes.map(recipe => <Recipe
+                                recipe={recipe}
+                                key={recipe.id}
+                            ></Recipe>)
+                        }
+
+                    </tbody>
+                </table>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import route from './Components/Routes/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
+  <RouterProvider router={route}/>
 )
