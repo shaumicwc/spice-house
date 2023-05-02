2a1a2c708341d01c2ee1c0de11407fd666817fbf
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-02 08:21:49 +0600

    chef card updated

--- src/Components/Pages/Home/ChefCard/ChefCard.jsx
+++ src/Components/Pages/Home/ChefCard/ChefCard.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { ArrowRightIcon} from '@heroicons/react/24/solid'
+import { Link } from 'react-router-dom';
 
-const ChefCard = () => {
+const ChefCard = ({ cf }) => {
+    const { id, name, picture, yearsOfExperience, numberOfRecipes, likes} = cf
+
     return (
-        <div>
-            
+        <div className="w-96 glass rounded-xl">
+            <img className='rounded-t-xl w-full h-64' src={picture} alt="car!" />
+            <div className="card-body h-46">
+                <h2 className="card-title">{name}</h2>
+                <div className='mt-2'>
+                <p className='font-semibold'>Years of experience : <span className='font-normal'>{yearsOfExperience} years</span></p>
+                <p className='font-semibold'>Number of Recipes : <span className='font-normal'>{numberOfRecipes} Recipes</span></p>
+                </div>
+                <div className="mt-5 mb-0 flex justify-between items-center">
+                    <p className='font-semibold'>Ratings: <span className='font-normal'>{likes}</span></p>
+                    <Link to={`/chefRecipe/${id}`}><button className="bg-btn-color py-2 px-4 rounded-md font-semibold flex items-center">View Recipes<ArrowRightIcon className='h-5 w-5 ml-2'/></button></Link>
+                </div>
+            </div>
         </div>
     );
 };
