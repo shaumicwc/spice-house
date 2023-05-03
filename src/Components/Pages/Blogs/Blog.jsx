e2f41a4b3b332083a3f1edcc20bcb4c21f641267
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-03 15:31:11 +0600

    blog updated

--- src/Components/Pages/Blogs/Blog.jsx
+++ src/Components/Pages/Blogs/Blog.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import './Blog.css'
+import { ArrowDownTrayIcon} from '@heroicons/react/24/solid';
+import html2pdf from 'html2pdf.js';
+
+/**please install: npm install html2pdf.js */
+
 const Blog = () => {
+
+    const downloadAsPdf = () =>{
+        const element = document.getElementById('blog');
+        const options = {
+          margin: 0.5,
+          filename: 'blog.pdf',
+          jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+        };
+        html2pdf().set(options).from(element).save();
+      };
     return (
-        <div className="blog-container">
+        <div className='py-10'>
+            <div className='flex justify-around items-center'>
+            <p className='text-center my-5 font-blog-qst-font-family font-extrabold text-6xl'>Q&A</p>
+            <button className='flex items-center text-xl font-semibold bg-blog-ans-color py-2 px-4 rounded-md' onClick={downloadAsPdf}>Download PDF<ArrowDownTrayIcon className='h-6 w-6 ml-3'/></button>
+            </div>
+            <div id='blog' >
         {/* Question : 1 section */}
         <div>
             <p>
-                <span className="question">Question : 1</span>
+                <span className="font-blog-q st-font-family mb-5 text-blog-qst-color text-2xl font-extrabold ">Question : 1</span>
                 <br /> 
-                <span className='qus'>Tell us the differences between uncontrolled and controlled components. </span>
+                <span className='qus font-blog-qus-font-family font-extrabold text-xl'>Tell us the differences between uncontrolled and controlled components. </span>
             </p>
             <p>
-                <span className="answer">Answer:</span>
+                <span className="text-blog-ans-color font-extrabold text-xl font-blog-qst-font-family mb-5">Answer:</span>
                 <br />
                 In React, components can handle user input in two ways: controlled or uncontrolled. They are:
             </p>
@@ -25,12 +44,12 @@ const Blog = () => {
         {/* Question : 2 section */}
         <div>
             <p>
-                <span className="question">Question : 2 </span>
+                <span className="font-blog-qst-font-family mb-5 text-blog-qst-color text-2xl font-extrabold ">Question : 2 </span>
                 <br /> 
-                <span className='qus'>How to validate React props using PropTypes? </span>
+                <span className='qus font-blog-qus-font-family font-extrabold text-xl'>How to validate React props using PropTypes? </span>
             </p>
             <p>
-                <span className="answer">Answer:</span>
+                <span className="text-blog-ans-color font-extrabold text-xl font-blog-qst-font-family mb-5">Answer:</span>
                 <br />
                 React PropTypes is a tool that help us check if the data (props) we are passing into a component is the correct type and format. It's like a spell-checker that prevents us from making mistakes. By using PropTypes, we can catch errors and bugs early in the development process, which can save us time and effort in the long run.
             </p>
@@ -39,12 +58,12 @@ const Blog = () => {
         {/* Question : 3 section */}
         <div>
             <p>
-                <span className="question">Question : 3</span>
+                <span className="font-blog-qst-font-family mb-5 text-blog-qst-color text-2xl font-extrabold ">Question : 3</span>
                 <br /> 
-                <span className='qus'>Tell us the difference between nodejs and express js. </span>
+                <span className='qus font-blog-qus-font-family font-extrabold text-xl'>Tell us the difference between nodejs and express js. </span>
             </p>
             <p>
-                <span className="answer">Answer:</span>
+                <span className="text-blog-ans-color font-extrabold text-xl font-blog-qst-font-family mb-5">Answer:</span>
                 <br />
                 Node.js is the underlying technology that allows you to run JavaScript code on the server, while Express.js is a framework that sits on top of Node.js and provides a set of tools and features for building web applications and APIs. Here are some key differences between Node.js and Express.js:
             </p>
@@ -60,12 +79,12 @@ const Blog = () => {
         {/* Question : 4 section */}
         <div>
             <p>
-                <span className="question">Question : 4</span>
+                <span className="font-blog-qst-font-family mb-5 text-blog-qst-color text-2xl font-extrabold ">Question : 4</span>
                 <br /> 
-                <span className='qus'>What is a custom hook, and why will you create a custom hook? </span>
+                <span className='font-blog-qus-font-family font-extrabold text-xl'>What is a custom hook, and why will you create a custom hook? </span>
             </p>
             <p>
-                <span className="answer">Answer: </span>
+                <span className="text-blog-ans-color font-extrabold text-xl font-blog-qst-font-family mb-5">Answer: </span>
                 <br />
                 A custom hook is a function in React that allows us to reuse stateful logic between components, making it easier to create reusable and composable code. It's a way to extract and reuse common code that manages stateful logic and side effects, by leveraging existing React hooks.Its very important that we use in time of need. 
 
@@ -74,11 +93,10 @@ const Blog = () => {
                I will create a custom hook for tasting and share components. Creating a custom hook in React can help me to improve reusability, organization, abstraction, and testing of my code. It allow me to encapsulate complex logic and state management into a reusable function, which can be easily shared across components and projects.
             </p>
         </div>
+            </div>
+
     </div>
     );
 };
 
 export default Blog;
-
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import route from './Components/Routes/Routes.jsx'
+import AuthProvider from './Components/Provider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={route}/>
+  <AuthProvider>
+    <RouterProvider router={route}/>
+  </AuthProvider>
 )
